52c4ae46e21967758e55fde23e20c7df7dfaca30
xinsen <xinsen.zhang.0571@gmail.com>
2020-12-22 23:18:07 +0800

    自顶向下分数计算

--- models/layers/crf.py
+++ models/layers/crf.py
@@ -1,10 +1,12 @@
 import torch
 from torch import nn
+from typing import Optional
 
 
 class CRF(nn.Module):
 
-    def __init__(self, num_tags: int, batch_first: bool = False) -> None:
+    def __init__(self, num_tags: int,
+                 batch_first: Optional[bool] = False) -> None:
         """初始化 CRF 层
 
         Args:
@@ -35,3 +37,52 @@ class CRF(nn.Module):
         nn.init.uniform_(self.start_transitions, -0.1, 0.1)
         nn.init.uniform_(self.end_transitions, -0.1, 0.1)
         nn.init.uniform_(self.transitions, -0.1, 0.1)
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}(num_tags={self.num_tags})'
+
+    def forward(self, emissions: torch.Tensor,
+                tags: torch.LongTensor,
+                mask: Optional[torch.ByteTensor] = None,
+                reduction: str = "mean") -> torch.Tensor:
+        if reduction not in ["none", "sum", "mean", "token_mean"]:
+            raise ValueError(
+                'reduction expected  "none", "sum", "mean", "token_mean",' +
+                f'but got {reduction}')
+        if mask is None:
+            mask = torch.ones_like(tags, dtype=torch.uint8, device=tags.device)
+        if mask.dtype != torch.uint8:
+            mask = mask.byte()
+        self._validation(emissions, tags, mask)
+
+        if self.batch_first:
+            emissions = emissions.transpose(0, 1)
+            tags = tags.transpose(0, 1)
+            mask = mask.transpose(0, 1)
+            # shape: (seq_length, batch_size, num_tags)
+
+        numerator = self._compute_score(emissions, tags, mask)
+        denominator = self._compute_normalizer(emissions, mask)
+        llh = numerator - denominator  # log likelihood
+
+        if reduction == 'none':
+            return llh
+        if reduction == 'sum':
+            return llh.sum()
+        if reduction == 'mean':
+            return llh.mean()
+        return llh.sum()/mask.float().sum()  # token level mean
+
+    def _validation(self, emissions: torch.Tensor,
+                    tags: torch.LongTensor,
+                    mask: Optional[torch.ByteTensor] = None) -> None:
+        pass
+
+    def _compute_score(self, emissions: torch.Tensor,
+                       tags: torch.LongTensor,
+                       mask: torch.ByteTensor) -> torch.Tensor:
+        pass
+
+    def _compute_normalizer(emissions: torch.Tensor,
+                            mask: torch.ByteTensor) -> torch.Tensor:
+        pass
