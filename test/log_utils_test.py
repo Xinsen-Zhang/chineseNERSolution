006c39952eecb478433a3b4dbb5006798bf70381
xinsen <xinsen.zhang.0571@gmail.com>
2020-12-28 17:01:21 +0800

    添加日志 utils

---
+++ test/log_utils_test.py
@@ -0,0 +1,11 @@
+import sys
+import os
+
+sys.path.append(os.path.join(os.getcwd(), "../"))
+
+from utils.log_utils import init_logger
+
+if __name__ == "__main__":
+    logger = init_logger('test', './data')
+    logger.info("test")
+    logger.error("test2")
